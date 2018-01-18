c8c8f857cd2de7d01a5c790a22ac643a6b962436
Chris Pham <phamchr@onid.orst.edu>
2018-01-18 10:47:56 -0800

    Add Constants

--- ground_station/resources/core/mapping.py
+++ ground_station/resources/core/mapping.py
@@ -8,3 +8,19 @@ import PIL.Image
 from io import StringIO, BytesIO
 import os
 import time
+
+#####################################
+# Constants
+#####################################
+_KEY =  
+
+# Number of pixels in half the earth's circumference at zoom = 21
+_EARTHPIX = 268435456
+# Number of decimal places for rounding coordinates
+_DEGREE_PRECISION = 4 
+# Larget tile we can grab without paying
+_TILESIZE = 640
+# Fastest rate at which we can download tiles without paying
+_GRABRATE = 4
+# Pixel Radius of Earth for calculations
+_pixrad = _EARTHPIX / math.pi
