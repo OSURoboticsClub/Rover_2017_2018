33694343176eaedff94334df4083b831ce1bc006
Chris Pham <phamchr@onid.orst.edu>
2018-02-08 13:33:32 -0800

    Changed file names and started initer for MapCoordinator

--- ground_station/resources/core/RoverMapCoordinator.py
+++ ground_station/resources/core/RoverMapCoordinator.py
@@ -8,7 +8,7 @@ import logging
 import rospy
 
 # Custom Imports
-import MapHelper
+import RoverMap
 
 #####################################
 # Global Variables
@@ -24,3 +24,31 @@ class RoverMapCoordinator(QtCore.QThread):
 
         self.shared_objects = shared_objects
         self.right_screen = self.shared_objects["screens"]["left_screen"]
+
+        self.setings = QtCore.QSettings()
+
+        self.logger = logging.getLogger("groundstation")
+
+        self.run_thread_flag = True
+        self.setup_map_flag = True
+
+        # setup map
+        self._get_map()
+        self._setup_map_threads()
+
+    def run(self):
+        self.logger.debug("Starting Map Coordinator Thread")
+        
+        while self.run_thread_flag:
+            self.msleep(10)
+
+        self.__wait_for_map_thread()
+        self.logger.debug("Stopping Map Coordinator Thread")
+
+    def __wait_for_map_thread(self):
+        self.map_thread.wait()
+
+    def _setup_map_threads(self):
+        self.map_thread = RoverMap.GMapsStitcher(2000, 
+                                                 2000, 44.567161, -123.278432,
+                                                 18, 'terrain', None, 20)
