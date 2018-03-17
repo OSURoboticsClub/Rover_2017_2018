b0b7d05fced36fa4a2f967e7cf660b7ba542c32a
Chris Pham <phamchr@onid.orst.edu>
2018-03-17 15:43:54 -0700

    added a 't' and fake values for adding from gps

--- software/ros_packages/ground_station/src/Framework/NavigationSystems/WaypointsCoordinator.py
+++ software/ros_packages/ground_station/src/Framework/NavigationSystems/WaypointsCoordinator.py
@@ -27,7 +27,7 @@ class WaypointsCoordinator(QtCore.QThread):
 
         # Nav Table Buttons
         self.nav_set_label = (self.left_screen.
-                              navigation_waypoints_set_buton)
+                              navigation_waypoints_set_button)
         self.nav_add_manual_label = (self.left_screen.
                                      navigation_waypoints_add_manual_button)
         self.nav_add_gps_label = (self.left_screen.
@@ -47,7 +47,7 @@ class WaypointsCoordinator(QtCore.QThread):
         self.new_manual_waypoint_entry.connect(self.update_manual_entry)
 
         # setting up signals to save for Navigation Table
-        self.nav_add_gps_label.onClick(self._nav_add_gps)
+        self.nav_add_gps_label.clicked(self._nav_add_gps)
 
         self.navigation_label.cellClicked.connect(self._on_nav_clicked)
         self.landmark_label.cellClicked.connect(self._on_land_clicked)
@@ -63,8 +63,10 @@ class WaypointsCoordinator(QtCore.QThread):
     def _nav_add_gps(self):
         # request GPS data
         name = self.navigation_label.rowCount()
-
-        self._add_to_table(name, lat, lng,)
+        lat = 44.567200
+        lng = -123.27860
+        distance = 200
+        self._add_to_table(name, lat, lng, distance, self.navigation_label)
 
     def setup_signals(self, start_signal,
                       signals_and_slots_signal, kill_signal):
@@ -83,7 +85,6 @@ class WaypointsCoordinator(QtCore.QThread):
         self.longitude_label.setValue(lng)
 
     def _on_nav_clicked(self, row, col):
-        print "nav" + str(row), str(col)
         self.new_manual_waypoint_entry.emit(
             self.navigation_label.item(row, 0).text(),
             float(self.navigation_label.item(row, 1).text()),
@@ -92,7 +93,6 @@ class WaypointsCoordinator(QtCore.QThread):
         )
 
     def _on_land_clicked(self, row, col):
-        print "land" + str(row), str(col)
         self.new_manual_waypoint_entry.emit(
             self.landmark_label.item(row, 0).text(),
             float(self.landmark_label.item(row, 1).text()),
