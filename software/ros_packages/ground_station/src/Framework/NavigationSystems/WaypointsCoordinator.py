25cce55bf3cade3363a26ce906d5448bca6176ee
Chris Pham <phamchr@onid.orst.edu>
2018-03-17 12:47:06 -0700

    creating interface glue for waypoints

---
+++ software/ros_packages/ground_station/src/Framework/NavigationSystems/WaypointsCoordinator.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtCore, QtWidgets, QtGui
+
+import logging
+
+import rospy
+
+class WaypointsCoordinator(QtCore.QThread):
+    
+
+
+    def connect_signals_and_slots(self):
+        self.
