a2ad3c2c9a43aeba8a0419087997820dc692e003
Corwin Perren <caperren@gmail.com>
2018-04-21 19:45:37 -0700

    Got simple package compiling with both ros and simplemotion included.

--- software/ros_packages/rover_arm/src/rover_arm.cpp
+++ software/ros_packages/rover_arm/src/rover_arm.cpp
@@ -4,6 +4,27 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 
+#include "simplemotion/simplemotion.h"
+
+
+using namespace std;
+
+int device_address = 1;
+
 int main(int argc, char** argv){
-    std::cout << "Test" << std::endl;
+        smbus busHandle = smOpenBus("/dev/ttyUSB0");
+
+    if (busHandle >= 0) {
+        cout << "Successfully connected bus" << endl;
+//        deviceAddress=ui->deviceAddress->value();
+    } else
+        cout << "Couldn't connect to bus";
+
+    while(1){
+        smSetParameter(busHandle, device_address, SMP_FAULTS, 0);
+        smSetParameter(busHandle, device_address, SMP_CONTROL_BITS1, SMP_CB1_ENABLE);
+        smSetParameter(busHandle, device_address, SMP_ABSOLUTE_SETPOINT, 1000);
+        cout << "Test" << endl;
+    }
+
 }
