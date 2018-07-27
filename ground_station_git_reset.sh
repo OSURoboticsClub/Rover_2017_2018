63454f22d13d32611ed547b840c921391017a428
Corwin Perren <caperren@gmail.com>
2018-07-27 14:37:34 -0700

    Made it copy files instead of folder.

--- ground_station_git_reset.sh
+++ ground_station_git_reset.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cp -r software/ros_packages/ground_station/src/Resources/Maps ~/Maps_Backup
+test -d ~/Maps_Backup || mkdir -p ~/Maps_Backup 
+cp -r software/ros_packages/ground_station/src/Resources/Maps/* ~/Maps_Backup
 git reset --hard
 git clean -fdx
 git pull
-cp -r ~/Maps_Backup software/ros_packages/ground_station/src/Resources/Maps
+test -d ~/software/ros_packages/ground_station/src/Resources/Maps || mkdir -p software/ros_packages/ground_station/src/Resources/Maps 
+cp -r ~/Maps_Backup/* software/ros_packages/ground_station/src/Resources/Maps/
